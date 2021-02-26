6a892bc27e2a55ecca1f83d5db5e3a06e4ba6eb9
Blaire Baker <bakerblaire@gmail.com>
2021-02-26 09:59:51 -0600

    solidified datetime format

--- src/components/AppointmentCard.js
+++ src/components/AppointmentCard.js
@@ -4,14 +4,11 @@ import { utcToZonedTime } from 'date-fns-tz'
 
 const AppointmentCard = ({ appointment, toggleModal, setSelectedAppointment }) => {
     const center = appointment.vaccination_center
-    // const date = appointment.appointment_time.toLocaleDateString()
-    // const time = appointment.appointment_time.toLocaleTimeString()
     const dateTime = utcToZonedTime(new Date(appointment.appointment_time), 'America/Chicago')
     
-    //make sure appointment time converts to central time 
     return (
         <div>
-            <h3>{dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}</h3> 
+            <h3>{format(dateTime, 'MMMM d, Y h:mmaaa')}</h3> 
             <h4>{center.name}</h4>
             <p>{center.address}</p>
             <p>{center.city} {center.state} {center.zip_code}</p>
