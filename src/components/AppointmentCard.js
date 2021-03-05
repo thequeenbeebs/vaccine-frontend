ff4ab1144fc1ef36a6f51c38c9624538fafcb21e
Blaire Baker <bakerblaire@gmail.com>
2021-03-05 13:00:59 -0600

    buttongroup addition

--- src/components/AppointmentCard.js
+++ src/components/AppointmentCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { format } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz'
-import Button from '@material-ui/core/Button';
+import { ButtonGroup, Button } from '@material-ui/core';
 
 const AppointmentCard = ({ appointment, toggleModal, setSelectedAppointment, openEditForm }) => {
     const center = appointment.vaccination_center
@@ -14,16 +14,18 @@ const AppointmentCard = ({ appointment, toggleModal, setSelectedAppointment, ope
             <p>{center.address}</p>
             <p>{center.city} {center.state} {center.zip_code}</p>
             <p>{center.phone_number}</p>
-            <Button variant="outlined"
-                onClick={() => {
-                setSelectedAppointment(appointment)
-                openEditForm()
-            }}>Edit</Button>
-            <Button variant="outlined"
-                onClick={() => {
-                toggleModal()
-                setSelectedAppointment(appointment)
-            }}>Cancel</Button>
+            <ButtonGroup>
+                <Button variant="outlined"
+                    onClick={() => {
+                    setSelectedAppointment(appointment)
+                    openEditForm()
+                    }}>Edit</Button>
+                <Button variant="outlined"
+                    onClick={() => {
+                    toggleModal()
+                    setSelectedAppointment(appointment)
+                    }}>Cancel</Button>
+            </ButtonGroup>
         </div>
     )
 }
